c4ca278e86144abc46b4bd142f0c7e5cf45643ef
Marcel Kalveram <marcel@hanno.co>
2018-03-05 20:58:03 +0100

    add modal

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,20 @@
-$(document).ready(function() {
+/* eslint-disable */
 
-  // Place JavaScript code here...
+$(document).ready(function () {
+  
+  $('.js-send-notification').on('click', function() {
+
+    var $row = $(this).closest('tr');
+    var name = $row.find('td.name').text().trim();
+
+    $('.modal').modal('show');
+    $('.modal .modal-title span').text(name);
+
+  });
+
+  $('.js-send-notification-submit').on('click', function() {
+    
+    $('.modal').modal('hide');
+  });
 
 });
